517c7318116a05bf261b4e11331fb97f56d6baf4
Tomek <tomek.wojak796@gmail.com>
2026-09-24 16:43:41 +0200

    fixed asides height problem

--- app/(site)/logowanie/page.tsx
+++ app/(site)/logowanie/page.tsx
@@ -35,6 +35,7 @@ export default function LoginPage() {
 			}
 
 			router.push("/dashboard");
+			router.refresh();
 			setError("");
 		} catch (err) {
 			console.log(err);
--- components/DashboardAside.tsx
+++ components/DashboardAside.tsx
@@ -80,7 +80,7 @@ export default function DashboardAside({ asideOpen }: { asideOpen: boolean }) {
 	return (
 		<>
 			<aside
-				className={`flex flex-col overscroll-contain col-start-1 col-end-2 w-70 ${asideOpen ? "translate-x-0" : "-translate-x-full"} bg-linear-to-b from-panel to-main fixed inset-y-0 md:static md:translate-x-0 left-0 h-screen border-r border-accent/20 transition-transform duration-200 md:transition-none z-50 shrink-0`}>
+				className={`flex flex-col overscroll-contain col-start-1 col-end-2 w-70 h-dvh ${asideOpen ? "translate-x-0" : "-translate-x-full"} bg-linear-to-b from-panel to-main fixed inset-y-0 md:static md:translate-x-0 left-0 border-r border-accent/20 transition-transform duration-200 md:transition-none z-50 shrink-0`}>
 				<div className="aside-header overflow-hidden p-4 flex items-center h-17 border-b border-accent/20">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
